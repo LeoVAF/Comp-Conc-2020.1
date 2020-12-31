dbaf2934dd3d90c8071dd1aa43b23c79ef152b09
Leonardo Veiga <75226985+LeoVAF@users.noreply.github.com>
2020-12-31 00:41:15 -0300

    Update trigonometricas.c

--- Trabalho 1/trigonometricas.c
+++ Trabalho 1/trigonometricas.c
@@ -2,20 +2,19 @@
 #include<stdlib.h>
 #include<math.h> // Compilar com -lm
 #include<pthread.h> // Compilar com -lpthread
+#include<time.h>
 
 long int NTHREADS; // Número de threads
 long int N; // Número de iterações
-double* sen; // Ponteiro para guardar os retornos das threads
-double x; // Parâmetro da função trigonométrica seno
+double* trigonometrica; // Ponteiro para guardar os retornos das threads
+double x; // Parâmetro da função trigonométrica
 
-// Calcula o fatorial
-double fatorial(long int n){
-	if(n < 2) return 1;
-	double fat = 1;
-	for(long int i = 1; i <= n; i++){
-		fat *= i;
-	}
-	return fat;
+// Função que calcula os termos das séries seno e cosseno
+double termo(double x ,long int n){
+	double a = 1;
+	for(long int i = 1; i <= n; i++) a *= x/i;
+	double b = a*x/(n+1)*x/(n+2);
+	return a-b;
 }
 
 // Limita x no intervalo [-2pi, 2pi]
@@ -32,6 +31,11 @@ double normaliza(double y){
 	return norma;
 }
 
+// Retorna o valor absoluto
+double absoluto(double x){
+	return (x < 0)? -x : x;
+}
+
 // Série para o cálculo de seno
 void* seno(void* arg){
 	long int id = *(long int *)arg;
@@ -40,45 +44,102 @@ void* seno(void* arg){
 	long int inic, fim;
 	inic = N*id/NTHREADS;
 	fim = N*(id+1)/NTHREADS-1;
+	
 	// Somatório resultante
 	double soma = 0;
-	for(long int i = inic; i <= fim; i++){ soma += pow(x, 4*i + 1)/fatorial(4*i + 1) - pow(x, 4*i + 3)/fatorial(4*i + 3);}
+	for(long int i = inic; i <= fim; i++){
+		soma += termo(x, 4*i + 1); // termo_seno da série
+	}
+	
 	// Armazena o somatório
-	sen[id] = soma;
+	trigonometrica[id] = soma;
 	pthread_exit(NULL);
 }
 
+// Série para o cálculo do cosseno
+void* cosseno(void* arg){
+	long int id = *(long int *)arg;
+	 
+	// Intervalo para as threads
+	long int inic, fim;
+	inic = N*id/NTHREADS;
+	fim = N*(id+1)/NTHREADS-1;
+	
+	// Somatório resultante
+	double soma = 0;
+	for(long int i = inic; i <= fim; i++){
+		soma += termo(x, 4*i+4);
+	}
+	// Armazena o somatório
+	trigonometrica[id] = soma;
+	pthread_exit(NULL);
+}
+
+
+
 int main(int argc, char* argv[]){
 	if(argc != 3){
-		puts("FORMATO: ./prog <número de iterações> <número de threads>");
+	puts("FORMATO: ./prog <número de iterações> <número de threads>");
 		return -1;
 	}
 	
 	// Dados iniciais
+	typedef void* (*Func)(void*);
+	Func f; // Ponteiro para threads
+	char resposta[32]; // resposta do usuário
+	int t = 0; // Variável de teste para a resposta
+	clock_t ti, tf; // Variáveis de marcação de tempo
 	N = atoll(argv[1]); // Número de iterações
 	NTHREADS = atoll(argv[2]); // Número de threads
 	if(NTHREADS > N) NTHREADS = N; // Limita o número de threads
 	pthread_t tid[NTHREADS]; // Identificador da thread
 	long int id[NTHREADS]; // Identificador da thread no programa
-	sen = (double*) malloc(NTHREADS*sizeof(double)); // Valor de retorno das threads
+	trigonometrica = (double*) malloc(NTHREADS*sizeof(double)); // Valor de retorno das threads
+	double resultado, tempo; // Resultado da série e duração das operações
 	
-	char resposta[32];
+	// Escolha da função
+	puts("Digite \"sen\" ou \"cos\" para escolher uma função:");
+	scanf("%s", resposta);
+	if(!strcmp(resposta, "cos")){
+		puts("Função cosseno selecionada");
+		f = &cosseno;
+	}
+	else{
+		puts("Função seno selecionada");
+		f = &seno;
+		t = 1;
+	}
+	
+	// Ciclo para cálculo da função escolhida
 	while(1){
-		puts("Digite o ponto (em graus) para cáculo da função seno: (digite s para sair)");
+		ti = clock();// Início da contagem
+		puts("Digite o ponto (em graus) para cáculo da função: (digite \"s\" para sair, \"sen\" ou \"cos\" para trocar de função)");
 		scanf("%s", resposta);
 		fflush(stdin); // Limpa o buffer do teclado
 		if(!strcmp(resposta, "s") || !strcmp(resposta, "S")) break;
-		
+		else if(!strcmp(resposta, "cos")){
+			puts("Função cosseno selecionada");
+			f = &cosseno;
+			t = 0;
+			continue;
+		}
+		else if(!strcmp(resposta, "sen")){
+			puts("Função seno selecionada");
+			f = &seno;
+			t = 1;
+			continue;
+		}
+			
 		x = normaliza((M_PI/180)*atof(resposta)); // Normaliza x devido limitação no fatorial
 		// Criação das threads
 		for(long int i = 0; i < NTHREADS; i++){
 			id[i] = i;
-			if(pthread_create(tid+i, NULL, seno, (void *) (id+i))){
+			if(pthread_create(tid+i, NULL, f, (void *) (id+i))){
 				fprintf(stderr, "Não foi possível criar thread: %li\n", i);
 				exit(1);
 			}
 		}
-	
+		
 		// Espera bloqueante
 		for(long int i = 0; i < NTHREADS; i++){
 			if(pthread_join(tid[i], NULL)){
@@ -86,11 +147,17 @@ int main(int argc, char* argv[]){
 				exit(1);
 			}
 		}
-	
-		double resultado = 0;
-		for(long int i = 0; i < NTHREADS; i++) resultado += sen[i];
-		printf("\nSérie = %.20lf\nERRO = %.20lf\n\n", resultado, resultado - sin(x));
+		
+		resultado = 0;
+		for(long int i = 0; i < NTHREADS; i++) resultado += trigonometrica[i];
+		tf = clock();
+		tempo = (double) (tf - ti)/CLOCKS_PER_SEC;
+		if(t) printf("\nSérie = %.20lf\nERRO = %.20lf\nTempo = %lf s\n\n", resultado, absoluto(resultado - sin(x)), tempo);
+		else{
+			resultado += 1 - x*x/2;
+			printf("\nSérie = %.20lf\nERRO = %.20lf\nTempo = %lf s\n\n", resultado, absoluto(resultado - cos(x)), tempo);
+		}
 	}
-	free(sen);
+	free(trigonometrica);
 	return 0;
 }
